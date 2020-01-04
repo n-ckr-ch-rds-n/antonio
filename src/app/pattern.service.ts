f7502a060e7309899c002fdcca252a574ab884b5
Nick <nick.richardson@rezonence.com>
2020-01-04 19:00:14 +0000

    Constrain type on pattern change emitter

--- src/app/pattern.service.ts
+++ src/app/pattern.service.ts
@@ -1,18 +1,18 @@
 import {EventEmitter, Injectable, Output} from '@angular/core';
 import {Beatmap} from './beatmap';
 import {PatternMode} from './pattern.mode';
-import {Synth} from 'tone';
+import {Instrument, Synth} from 'tone';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatternService {
   @Output()
-  patternChange = new EventEmitter<any>();
+  patternChange = new EventEmitter<{source: Instrument}>();
 
   constructor() { }
 
-  generatePattern(activeBeats: Beatmap, mode: PatternMode, source?: Synth) {
+  generatePattern(activeBeats: Beatmap, mode: PatternMode, source?: Instrument) {
     Object.keys(activeBeats).forEach(beat => {
       Object.keys(activeBeats[beat]).forEach(sixteenth => {
         activeBeats[beat][sixteenth] = mode === PatternMode.Clear ? false : Math.random() > 0.5;
